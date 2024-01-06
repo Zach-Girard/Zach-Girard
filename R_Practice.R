78c54c0eae4e77709a79f49c2659d18c61e47be1
Zach-Girard <155785220+Zach-Girard@users.noreply.github.com>
2024-01-05 19:34:29 -0700

    Update R_Practice.R
    change = to <- for assigning variables

--- R_Practice.R
+++ R_Practice.R
@@ -1,9 +1,9 @@
 print("Hello World")
 
-x = 1
-y = x + 1
+x <- 1
+y <- x + 1
 y
-z = x + y
+z <- x + y
 z
 
 
@@ -15,13 +15,13 @@ for(i in 1:5){
 
 
 
-vect_1 = c(1,2,3,4,5)
+vect_1 <- c(1,2,3,4,5)
 
 
 
 for(num in vect_1){
-  +   vect_2 = c(vect_2, num * 10)
-  + }
+  +   vect_2 = c(vect_2, num * 10) 
+  }
 vect_2
 
 
@@ -48,34 +48,34 @@ print(paste("Spring", month))
 
 
 
-my_list = list(c(1,2,3),c(4,5,6), c(7,8,9))
+my_list <- list(c(1,2,3),c(4,5,6), c(7,8,9))
 my_list
 
 
 
-x1 = 1:5
-x2 = letter[1:5]
-x3 = c(TRUE,FALSE,TRUE,FALSE,FALSE)
-dt_1= data.table(x1,x2,x3)
+x1 <- 1:5
+x2 <- letter[1:5]
+x3 <- c(TRUE,FALSE,TRUE,FALSE,FALSE)
+dt_1 <- data.table(x1,x2,x3)
 dt_1
 
 
 
 
-dt_2 = data.table("A"= x1,"B"= x2, "C"=x3)
+dt_2 <- data.table("A"= x1,"B"= x2, "C"=x3)
 dt_2
 
 
 
-dt_3= data.table("A" = rnorm(5), "B" = rpois(5,5), "C"= rbinom(5,2,0.6))
+dt_3 <- data.table("A" = rnorm(5), "B" = rpois(5,5), "C"= rbinom(5,2,0.6))
 dt_3
 
 
 
 
-bird_speed = rpois(5,5)
-Birds = c("Bluebird","Hawk","Sparrow","Robin","Eagle")
-bird_data = data.frame(name=Birds,value=bird_speed)
+bird_speed <- rpois(5,5)
+Birds <- c("Bluebird","Hawk","Sparrow","Robin","Eagle")
+bird_data <- data.frame(name=Birds,value=bird_speed)
 
 
 
